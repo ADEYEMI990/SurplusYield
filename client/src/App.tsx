d5e8a52cd139b415261f44eb4f423deaeccbd314
Adeyemi <adeyemiibrahim990@gmail.com>
2025-09-16 10:41:02 -0700

    Project Setup & Creating Folder structure

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      {/* <Router /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html','./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f766e',
+        accent: '#06b6d4'
+      }
+    }
+  },
+  plugins: []
+}
