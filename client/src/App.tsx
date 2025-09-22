b6512b39c5f48621f62d67176b146c71d211be19
Adeyemi <adeyemiibrahim990@gmail.com>
2025-09-22 03:13:14 -0700

    integrate toast notifications + React Query setup for API calls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,30 +9,29 @@ import SitePages from "./pages/admin/SitePages";
 import SiteNavigation from "./pages/admin/SiteNavigation";
 import SiteFooter from "./pages/admin/SiteFooter";
 import Settings from "./pages/admin/Settings";
-import { Toaster } from "react-hot-toast";
+import { queryClient } from "./services/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   return (
-    <>
-      <Toaster position="top-right" reverseOrder={false} />
-      {/* <Router /> */}
-       <BrowserRouter>
-      <Routes>
-        {/* Admin routes */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="plans" element={<Plans />} />
-          <Route path="transactions" element={<Transactions />} />
-          <Route path="referrals" element={<Referrals />} />
-          <Route path="rewards" element={<Rewards />} />
-          <Route path="site/pages" element={<SitePages />} />
-          <Route path="site/navigation" element={<SiteNavigation />} />
-          <Route path="site/footer" element={<SiteFooter />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-      </Routes>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          {/* Admin routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="plans" element={<Plans />} />
+            <Route path="transactions" element={<Transactions />} />
+            <Route path="referrals" element={<Referrals />} />
+            <Route path="rewards" element={<Rewards />} />
+            <Route path="site/pages" element={<SitePages />} />
+            <Route path="site/navigation" element={<SiteNavigation />} />
+            <Route path="site/footer" element={<SiteFooter />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
-    </>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
+import { ToastProvider } from "./components/common/ToastProvider";
+import './index.css';
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <ToastProvider />
   </React.StrictMode>
 );
